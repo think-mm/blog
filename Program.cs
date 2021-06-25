a6c5df9acd0b114b77fe6a1f27e589f53108ff59
code at mm <code@mukto-mona.com>
2021-06-25 10:20:14 -0700

    update Entry Point Program to sync with new static platform changes

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using System.Threading.Tasks;
 using Statiq.App;
+using Statiq.Common;
 using Statiq.Web;
 
-namespace MySite
+namespace mmblog
 {
-  public class Program
-  {
-    public static async Task<int> Main(string[] args) =>
-      await Bootstrapper
-        .Factory
-        .CreateWeb(args)
-        .RunAsync();
-  }
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .DeployToGitHubPagesBranch("think-mm", "blog", Config.FromSetting<string>("GITHUB_TOKEN"), "web")
+                .RunAsync();
+    }
 }
